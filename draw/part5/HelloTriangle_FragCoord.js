b755c812b32355a22dbcb5ea8246f0d2e83b4282
wangzongyao <wangzongyao@bytedance.com>
2021-10-09 15:04:09 +0800

    feat: 渐变色

---
+++ draw/part5/HelloTriangle_FragCoord.js
@@ -0,0 +1,82 @@
+// 顶点着色器
+const VSHADER_SOURCE = 
+  'attribute vec4 a_Position; \n' + 
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' + // Set the vertex coordinates of the point
+  '}\n';
+
+// 片元着色器
+const FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'uniform float u_Width;\n' +
+  'uniform float u_Height;\n' +
+  'void main() {\n' +
+  '  gl_FragColor = vec4(gl_FragCoord.x / u_Width, 0.0, gl_FragCoord.y / u_Height, 1.0);\n' + // Set the point color
+  '}\n';
+
+function main() {
+    // 获取canvas元素
+    const canvas = document.querySelector('#example');
+    if(!canvas) {
+        console.log('获取画布失败');
+        return;
+    }
+    // 获取gl绘图上下文
+    const gl = getWebGLContext(canvas, true);
+    if(!gl) {
+        console.log('获取gl失败');
+        return;
+    }
+    // 初始化着色器
+    if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('初始化着色器失败');
+        return;
+    }
+
+    // 设置顶点位置
+    const n = initVertexBuffers(gl);
+    if(n < 0) {
+        console.log('失败了去设置顶点位置');
+        return ;
+    }
+
+    // 指定清空canvas的颜色值
+    gl.clearColor(0.9, 0.9, 0.9, 1.0);
+    // 清空canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // 绘制三角形
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+function initVertexBuffers(gl) {
+    const vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ])
+    const n = 3;
+    const vertexBuffer = gl.createBuffer();
+    if(!vertexBuffer) {
+        console.log('创建失败');
+        return -1;
+    }
+    // 将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    // 向缓冲区写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    // 获取attribute变量的存贮位置
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if(a_Position < 0) {
+        console.log('失败了获取local storage');
+    }
+    // 将缓冲区分配给变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    // 连接a_Position变量与分配的缓冲区，处理缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+
+    const u_Width = gl.getUniformLocation(gl.program, 'u_Width');
+    const u_Height = gl.getUniformLocation(gl.program, 'u_Height');
+    gl.uniform1f(u_Width, gl.drawingBufferWidth);
+    gl.uniform1f(u_Height, gl.drawingBufferHeight);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    return n;
+}
